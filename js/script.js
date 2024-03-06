a2d6fe43b4c776d957ad44dc889bed29cc0ddf9f
Bianca G. Griffin <Biancagriffin@hotmail.es>
2024-03-06 21:49:17 +0100

    actualizacion2

--- js/script.js
+++ js/script.js
@@ -3,31 +3,39 @@
 
 let contador = document.getElementById('contadorVisitas');
 let clear = document.getElementById('btnReestablecer');
+//localStorage.setItem('contadorVisitas', viewers);
+//let viewers = localStorage.getItem('contadorVisitas')
 
 
-document.addEventListener('DOMContentLoaded' , ( )=>{
-      
-    let viewers = localStorage.getItem('contadorVisitas')
-       viewers = viewers || 0;
-       viewers ++;
-       localStorage.setItem('contadorVisitas' , viewers);
-       contador.textContent = viewers;
-
-
-
+//creo evento con html(document)
+document.addEventListener('DOMContentLoaded', () => {
+  
+ //obtiene valor almacenado en CV y se guarda en variable
+     let viewers = localStorage.getItem('contadorVisitas');
+     // se le asigna 0 al contador
+     viewers =viewers || 0; 
+     //incrementar cada vez +1
+     viewers++;
+     //actualiza el valor que se va almacenando
+     localStorage.setItem('contadorVisitas', viewers)
+     //actualiza con html con let'contador' contador de visitas
+     contador.textContent = viewers;
 
 
 })
 
-  
-
-clear.addEventListener('click', ()=> {
-
-  let viewers = 0;
-
-  localStorage.setItem('contadorVisitas', viewers);
 
 
-  contador.textContent = viewers;
-  });
 
+//crear evento boton restablecer
+
+clear.addEventListener ('click', () =>{
+  //poner valor a 0
+    let viewers = 0;
+    //actualiza el valor que se va almacenando
+    localStorage.setItem('contadorVisitas',viewers)
+    //poner a 0 el contador en html
+    contador.textContent= viewers;
+
+
+})
